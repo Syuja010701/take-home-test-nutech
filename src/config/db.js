2a5098e1e3eb13c5f04b0b091540b17a98d6d0e0
Syuja010701 <syujarajabmahiran2@gmail.com>
2025-11-12 18:06:52 +0700

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
-// CommonJS version
 require('dotenv').config();
 const mysql = require('mysql2/promise');
+const url = require('url');
 
+const dbUrl = new URL(process.env.DATABASE_URL);
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 3306,
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace('/', ''),
+  port: dbUrl.port,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 pool.getConnection()
@@ -21,5 +22,6 @@ pool.getConnection()
   .catch(err => {
     console.error('Error connecting to the database:', err);
   });
+
 
 module.exports = {pool};
